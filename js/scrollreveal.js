b238f153b061326d5d7ec69bcc6e7da869a62523
Maximo Apaza Chirhuana <53198604+maximopeoficiales@users.noreply.github.com>
2020-01-08 18:26:33 -0500

    correcion

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -9,18 +9,21 @@
         sr.reveal('.hero', {
             duration: 1000,
             origin: 'top',
-            distance: '300px' /* distancia desde arriba en este caso */
+            distance: '300px',/* distancia desde arriba en este caso */
+            viewFactor: 0.2
         });
 
         sr.reveal('.contenido_competencia', {
             duration: 1000,
             origin: 'right',
-            distance: '300px'
+            distance: '300px',
+            viewFactor: 0.2
         });
 
         sr.reveal('.educacion', {
             duration: 1000,
-            origin: 'bottom'
+            origin: 'bottom',
+            viewFactor: 0.2
         });
 
         sr.reveal('.proyectos div', {
