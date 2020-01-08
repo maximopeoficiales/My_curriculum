61a4bb041ba00b56c423f9f412854c80d9aab0dd
Maximo Apaza Chirhuana <53198604+maximopeoficiales@users.noreply.github.com>
2020-01-08 18:20:25 -0500

    solucion de hero

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -7,32 +7,31 @@
         });
 
         sr.reveal('.hero', {
-            duration: 2000,
+            duration: 1000,
             origin: 'top',
             distance: '300px' /* distancia desde arriba en este caso */
         });
 
         sr.reveal('.contenido_competencia', {
-            duration: 2000,
+            duration: 1000,
             origin: 'right',
             distance: '300px'
         });
 
         sr.reveal('.educacion', {
-            duration: 2000,
-            delay: 1000,
+            duration: 1000,
             origin: 'bottom'
         });
 
         sr.reveal('.proyectos div', {
-            duration: 2000,
+            duration: 1000,
             origin: 'left',
             distance: '300px',
             viewFactor: 0.2 /* factor de vision */
         });
 
         sr.reveal('.interes', {
-            duration: 2000,
+            duration: 1000,
             origin: 'top',
             distance: '300px',
             viewFactor: 0.2 /* factor de vision */
